ef684c27765f88f0ce2ceabb25296099aa3a86d0
Aiden Taylor <aiden.taylor@protonmail.com>
2023-04-10 02:28:48 -0600

    Deleted.

--- sources/tarottools/tarottools.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TAROTTOOLS_H
-#define _TAROTTOOLS_H
-
-int QueryToNumberConverter(char* Query);
-void TarotGenerator(int* Cards, int* Decks, unsigned int Question, int* Options);
-void TarotPrint(int* Cards, int* Decks, int* Options);
-
-#endif
