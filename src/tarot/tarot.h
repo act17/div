d0edd533f6665d56017d5aa5d9f2401a4c651af9
Aiden Taylor <aiden.taylor@protonmail.com>
2023-04-10 16:51:41 -0600

    Deleted.

--- src/tarot/tarot.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TAROTTOOLS_H
-#define _TAROTTOOLS_H
-
-int Gematria(char* Query, int Type);
-void TarotGenerator(int* Cards, int* Decks, unsigned int Question, int* Options);
-void TarotPrint(int* Cards, int* Decks, int* Options);
-
-#endif
